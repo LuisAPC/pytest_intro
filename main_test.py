61f7d7f319cc8f3a6905176f1d72bc8ed010e3bb
lplancarte <lplancarte@outlook.com>
2023-08-03 16:52:07 -0600

    Anadi un ejemplo usando tmpdir para un directorio temporal

--- main_test.py
+++ main_test.py
@@ -1,5 +1,5 @@
 import pytest
-from main import add
+from main import add, write
 
 def test_add():
     assert add(2, 2) == 4
@@ -15,3 +15,16 @@ def test_add():
 )
 def test_add_parametrize(input_x, input_y, expected):
     assert add(input_x, input_y) == expected
+
+#utilizar tmpdir para no tener que crear un archivo y despues borrarlo, esto lo hace automaticamente
+def test_write_tmp_dir(tmpdir):
+    data_in = "Soy Luis"
+    fpath = f"{tmpdir}/test_write_text.txt"
+    write(fpath, data_in)
+
+    with open(fpath) as file_out:
+        data_out = file_out.read()
+
+    assert data_in == "data_out"
+    # assert data_in == data_out
+
